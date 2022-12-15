a78304168dfd58e60ac180df2fc89dc6d788f1a9
Daniel G <104504435+ThatDudeDan@users.noreply.github.com>
2022-12-15 11:26:41 -0400

    Update QuestionAccessor.java

--- src/java/DB/QuestionAccessor.java
+++ src/java/DB/QuestionAccessor.java
@@ -1,8 +1,7 @@
 package DB;
 
 
-import db.ConnectionManager;
-import entity.ConnectString;
+import entity.Question;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,7 +11,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import entity.Quiz;
-public class SongItemAccessor {
+import java.util.Date;
+public class QuestionAccessor {
     private static PreparedStatement getByIDStatement = null;
     private static Connection conn = null;
     private static PreparedStatement selectAllStatement = null;
@@ -21,7 +21,7 @@ public class SongItemAccessor {
     private static PreparedStatement updateStatement = null;
 
     // constructor is private - no instantiation allowed
-    private SongItemAccessor() {
+    public QuestionAccessor() {
     }
 
     private static void init() throws SQLException {
@@ -29,7 +29,7 @@ public class SongItemAccessor {
             try {
                 conn = ConnectionManager.getConnection(ConnectString.getConnectionString(), ConnectString.getUser(), ConnectString.getPassword());
             } catch (ClassNotFoundException ex) {
-                Logger.getLogger(SongItemAccessor.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(QuestionAccessor.class.getName()).log(Level.SEVERE, null, ex);
             }
             getByIDStatement = conn.prepareStatement("select quizquestion.quizID,quizTitle, quizquestion.questionID, questionText, points from quizquestion \n" +
 "inner join quiz on quizquestion.quizID = quiz.quizID inner join\n" +
@@ -41,125 +41,30 @@ public class SongItemAccessor {
         }
     }
 //
-       private static List<SongItem> getQuizzesByQuery(String selectString) {
-        $result = [];
+       private static List<Question> getQuestionsByQuery(String selectString) {
+        List<Question> result = new ArrayList<>();
 
         try {
-            $quizAccess = new questionAccessor();
-            //$stmt = $this->conn->prepare($selectString);
-            $stmt = $this->conn->prepare($selectString);
-            $stmt->execute();
-            $dbresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            foreach ($dbresults as $r) {
-                $quizID = $r['quizID'];
-                $quizTitle = $r['quizTitle'];
-                $quizQuestions = $quizAccess->getQuestionsfromQuiz($quizID);
-                $quizPoints = $this->getQuizPoints($quizID);
-                $obj = new Quiz($quizID, $quizTitle, $quizQuestions, $quizPoints);
-                array_push($result, $obj);
+            init();
+            PreparedStatement stmt = conn.prepareStatement(selectString);
+            ResultSet results = stmt.executeQuery();
+            while(results.next()){
+                String id = results.getString("questionID");
+                String text = results.getString("questionText");
+                String choices = results.getString("choices");
+                int answer = results.getInt("answer");
+                result.add(new Question(id, text, choices, answer));
             }
         }
-        catch (Exception $e) {
-            $result = [];
-        }
-        finally {
-            if (!is_null($stmt)) {
-                $stmt->closeCursor();
-            }
-        }
-
-        return $result;
-    }
-
-    public function getAllQuizzes() {
-        return $this->getQuizzesByQuery("select * from Quiz");
-    }
-    //
-
-    public static List<SongItem> getAllSongItems() {
-        List<SongItem> songs = new ArrayList();
-        try {
-            init();
-            ResultSet rs = selectAllStatement.executeQuery();
-            while (rs.next()) {
-                int id = rs.getInt("songID");
-                String title = rs.getString("title");
-                String artist = rs.getString("Artist");
-                int price = rs.getInt("price");
-                boolean isSold = rs.getBoolean("isSold");
-                SongItem item = new SongItem(id, title, artist, price, isSold);
-                songs.add(item);
-            }
-        } catch (SQLException ex) {
-            songs = new ArrayList();
-            songs.add(new SongItem(1,ex.getMessage(),"dummy",12,false));
-        }
-        return songs;
-    }
-
-    public static boolean deleteItem(SongItem item) {
-        boolean res;
-
-        try {
-            init();
-            deleteStatement.setInt(1, item.getId());
-            int rowCount = deleteStatement.executeUpdate();
-            res = (rowCount == 1);
-        } catch (SQLException ex) {
-            res = false;
-        }
-        return res;
-    }
-
-
-    public static boolean deleteItemById(int id) {
-        SongItem dummy = new SongItem(id, "title", "artist", 0, false);
-        return deleteItem(dummy);
-    }
-    
-    public static boolean insertItem(SongItem item) {
-        boolean res;
-        
-        try {
-            init();
-            insertStatement.setInt(1, item.getId());
-            insertStatement.setString(2, item.getTitle());
-            insertStatement.setString(3, item.getArtist());
-            insertStatement.setInt(4, item.getPrice());
-            insertStatement.setBoolean(5, item.issold());
-            int rowCount = insertStatement.executeUpdate();
-            res = (rowCount == 1);
-        }
-        catch (SQLException ex) {
-            res = false;
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+            result = new ArrayList<>();
         }
         
-        return res;
+        return result;
     }
 
-    public static boolean updateItem(SongItem item) {
-        boolean res;
-        
-        try {
-            init();
-            System.out.print(item.getArtist());
-            System.out.print(item.getTitle());
-            System.out.print(item.getPrice());
-
-            updateStatement.setString(1, item.getTitle());
-            updateStatement.setString(2, item.getArtist());
-            updateStatement.setInt(3, item.getPrice());
-            updateStatement.setBoolean(4, item.issold());
-            updateStatement.setInt(5, item.getId());
-            int rowCount = updateStatement.executeUpdate();
-            res = (rowCount == 1);
-        }
-        catch (SQLException ex) {
-            System.out.print(ex.getMessage());
-            res = false;
-        }
-        
-        return res;
+    public List<Question> getAllQuestion() {
+        return getQuestionsByQuery("Select * from question");
     }
-
-} 
+}
