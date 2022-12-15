20d0d45ae6e40fcbd826c7e907c1bb9942e17f31
Richard Clark <98236451+Fade253@users.noreply.github.com>
2022-12-15 15:38:09 -0400

    megalovania

--- src/java/DB/QuestionAccessor.java
+++ src/java/DB/QuestionAccessor.java
@@ -1,6 +1,9 @@
 package DB;
 
 
+import DB.QuestionAccessor;
+import DB.ConnectionManager;
+import DB.ConnectString;
 import entity.Question;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,8 +14,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import entity.Quiz;
-import java.util.Date;
-public class QuestionAccessor {
+import entity.QuizQuestion;
+public class QuizAccessor {
     private static PreparedStatement getByIDStatement = null;
     private static Connection conn = null;
     private static PreparedStatement selectAllStatement = null;
@@ -21,7 +24,7 @@ public class QuestionAccessor {
     private static PreparedStatement updateStatement = null;
 
     // constructor is private - no instantiation allowed
-    public QuestionAccessor() {
+    private QuizAccessor() {
     }
 
     private static void init() throws SQLException {
@@ -29,65 +32,61 @@ public class QuestionAccessor {
             try {
                 conn = ConnectionManager.getConnection(ConnectString.getConnectionString(), ConnectString.getUser(), ConnectString.getPassword());
             } catch (ClassNotFoundException ex) {
-                Logger.getLogger(QuestionAccessor.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(QuizAccessor.class.getName()).log(Level.SEVERE, null, ex);
             }
             getByIDStatement = conn.prepareStatement("select quizquestion.quizID,quizTitle, quizquestion.questionID, questionText, points from quizquestion \n" +
 "inner join quiz on quizquestion.quizID = quiz.quizID inner join\n" +
 "question on quizquestion.questionID = question.questionID where quiz.quizID = :quizID\"");
-            selectAllStatement = conn.prepareStatement("select * from songitems");
-            deleteStatement = conn.prepareStatement("delete from songitems where songID = ?");
-            insertStatement = conn.prepareStatement("insert into songitems values (?,?,?,?,?)");
-            updateStatement = conn.prepareStatement("update songitems set title = ?, Artist = ?, Price = ?, isSold = ? where songID = ?");
+            selectAllStatement = conn.prepareStatement("select * from Quiz");
+            
         }
     }
 //
-       private static List<Question> getQuestionsByQuery(String selectString) {
-        List<Question> result = new ArrayList<>();
+       private static List<Quiz> getQuizzesByQuery(String selectString) {
+        List<Quiz> result = new ArrayList<>();
 
         try {
             init();
-            PreparedStatement stmt = conn.prepareStatement(selectString);
-            ResultSet results = stmt.executeQuery();
-            while(results.next()){
-                String id = results.getString("questionID");
-                String text = results.getString("questionText");
-                String choices = results.getString("choices");
-                int answer = results.getInt("answer");
-                result.add(new Question(id, text, choices, answer));
+            //$stmt = $this->conn->prepare($selectString);
+            PreparedStatement prep = conn.prepareStatement(selectString);
+            ResultSet rs=prep.executeQuery();
+            while (rs.next()){
+                String id = rs.getString("quizID");
+                String title = rs.getString("quizTitle");
+                List<Question> q = QuestionAccessor.getQuestionsByID(id);
+                 List<Integer> points = QuizQuestionAccessor.getPoints(id);
+                Quiz item = new Quiz(id, title, q, points);
+                result.add(item);
             }
         }
-        catch (SQLException e){
-            System.out.println(e.getMessage());
-            result = new ArrayList<>();
+        catch (Exception e) {
+            e.getMessage();
         }
-        
+
         return result;
     }
 
-    public List<Question> getAllQuestion() {
-        return getQuestionsByQuery("Select * from question");
-    }
-     public List<Question> getQuestionsByID(String QuizID)
-    {
-   List<Question> result = new ArrayList<>();
+    public static List<Quiz> getAllQuizzes() {
+       List<Quiz> result = new ArrayList<>();
 
         try {
             init();
-            getByIDStatement.setString(1, QuizID);
-            ResultSet results = getByIDStatement.executeQuery();
-            while(results.next()){
-                String id = results.getString("questionID");
-                String text = results.getString("questionText");
-                String choices = results.getString("choices");
-                int answer = results.getInt("answer");
-                result.add(new Question(id, text, choices, answer));
+            PreparedStatement prep = conn.prepareStatement("select * from quiz");
+            ResultSet rs=prep.executeQuery();
+            while (rs.next()){
+                String id = rs.getString("quizID");
+                String title = rs.getString("quizTitle");
+                List<Question> q = QuestionAccessor.getQuestionsByID(id);
+                List<Integer> points = QuizQuestionAccessor.getPoints(id);
+                Quiz item = new Quiz(id, title, q, points);
+                //System.out.println(item.getQuestions());
+                result.add(item);
             }
         }
-        catch (SQLException e){
+        catch (SQLException e) {
             System.out.println(e.getMessage());
-            result = new ArrayList<>();
         }
-        
+
         return result;
     }
-}
+} 
