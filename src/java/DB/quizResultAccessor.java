4da68e36365b1fb74fd4678f6705e4b7d55253bb
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2022-12-16 14:49:32 -0400

    Quiz Result Accessor

--- src/java/DB/quizResultAccessor.java
+++ src/java/DB/quizResultAccessor.java
@@ -2,21 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-//package controller;
-//
-//
-//import db.ConnectionManager;
-//import entity.ConnectString;
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//import java.util.List;
-//import entity.SongItem;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-
 
 package DB;
 
@@ -47,7 +32,7 @@ public class quizResultAccessor {
     private static PreparedStatement deleteStatement = null;
     private static PreparedStatement insertStatement = null;
     private static PreparedStatement updateStatement = null; 
-    private static PreparedStatement insertStatementString = null;
+    
     public quizResultAccessor() {
     }
 
@@ -61,15 +46,7 @@ public class quizResultAccessor {
             
             selectAllStatement = conn.prepareStatement("select * from quizresult");
             getByIDStatement = conn.prepareStatement("select * from quizResult where resultID = ?");
-            //1: ResultID
-            //2: quizID
-            //3: username
-            //4: quizStartTime
-            //5: quizEndTime
-            //6: userAnswers
-            //7 Score Num
-            //8 score den
-            insertStatement = conn.prepareStatement("insert INTO quizResult values (?, ?, ?, ?, ?, ?, ?,?)");
+            
             } catch (SQLException ex) {
                 System.out.print(ex.getMessage() + "Lith");
             }
@@ -121,38 +98,6 @@ public class quizResultAccessor {
             }
             return allResults;
     }
-         public boolean insertResult(QuizResult obj) {
-            boolean success = false;
-            ArrayList<QuizResult> allResults = new ArrayList<QuizResult>();
-            if (!init())
-            {
-                return false;
-            }
-            try {
-                insertStatement.setString(1, obj.getId());
-                insertStatement.setString(2, obj.getQuiz());
-                insertStatement.setString(3, obj.getUser());
-                insertStatement.setTimestamp(4, obj.getStartTime());
-                insertStatement.setTimestamp(5, obj.getEndTime());
-                insertStatement.setString(6, obj.getUserAnswersString());
-                insertStatement.setInt(7, obj.getNum());
-                insertStatement.setInt(8, obj.getDen());
-
-                              
-                int bool = insertStatement.executeUpdate();
-                success = (bool == 1);
-            } catch(SQLException ex)
-            {
-               System.out.println("*************");
-                System.out.println("*************");
-                System.out.println("*****" + ex.getMessage() + "***");
-                System.out.println("*************");
-                System.out.println("*************"); 
-                success = false;
-            }
-            return success;
-    }
-
         
         public ArrayList<QuizResult> getQuizResultsByID(String myresultID) {
             init();
@@ -180,4 +125,4 @@ public class quizResultAccessor {
             }
             return allResults;
     }
-}
+
