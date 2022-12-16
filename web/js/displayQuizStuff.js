da9d2fd1f6e45ec0ee01852758ab73d5567d6427
Daniel G <104504435+ThatDudeDan@users.noreply.github.com>
2022-12-16 16:05:39 -0400

    Update displayQuizStuff.js

--- web/js/displayQuizStuff.js
+++ web/js/displayQuizStuff.js
@@ -5,9 +5,45 @@
 
 
 window.onload = function () {
+    let row = document.querySelectorAll("#doQuiz");
+    for (i = 0; i < row.length; i++)
+    {
+        document.querySelectorAll("#doQuiz")[i].addEventListener("click", sendResult);
+    }
     document.querySelector("#searchBtn").addEventListener("click", grabSearch);
     document.querySelector("#resetBtn").addEventListener("click", loadQuizes);
+    document.querySelector("#logoutBtn").addEventListener("click", logoutUser);
+    
 };
+function logoutUser()
+{
+    console.log("FOCAC");
+    let url = "logout/";
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            let resp = xmlhttp.responseText;
+            console.log(resp);
+            if (resp.includes("Out")) {
+                alert("Thank you for logging out.");
+                window.location.replace("index.html");
+
+            } else {
+                
+            }
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function sendResult(evt)
+{
+    let row = evt.target.parentElement.parentElement;
+    let id = row.querySelector("td").innerHTML;
+    console.log(id);
+    window.location.assign("QuizTake.jsp?quizid=" + id);
+}
 
 function grabSearch() {
     let type = document.querySelector("#searchChoice").value;
@@ -21,7 +57,7 @@ function grabSearch() {
     }
 }
 
-function searchText () {
+function searchText() {
     let value = document.querySelector("#searchInput").value;
     let con = /\w+/;
 
@@ -71,7 +107,7 @@ function searchID() {
     }
 }
 
-function loadQuizes () {
+function loadQuizes() {
     let url = "QuizService/Items";
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
@@ -81,7 +117,7 @@ function loadQuizes () {
             if (resp.search("ERROR") >= 0) {
                 alert("could not complete request");
             } else {
-              window.location.assign("DisplayQuizzes.jsp");
+                window.location.assign("DisplayQuizzes.jsp");
             }
         }
     };
