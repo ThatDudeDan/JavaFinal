611bd01390c5b1b7d78b74be28ff20b5f080e7cb
Richard Clark <98236451+Fade253@users.noreply.github.com>
2022-12-16 16:23:43 -0400

    Update QuizResult.js

--- web/js/QuizResult.js
+++ web/js/QuizResult.js
@@ -100,7 +100,7 @@ function buildQuiz()
         {
           text += "The user got the wrong answer.";
         }
-        text+="</div>
+        text+="</div>"
     }
     text += "<br>" +"<div id='ResultsScore'>" + "The total score of the quiz: " + results["scoreNumerator"] + "/" + results["scoreDenumerator"] + "</div>";
     text += "<br>" +"<div id='ResultsScore'>" + results["scoreNumerator"]  /  results["scoreDenumerator"] * 100 + "</div>";
